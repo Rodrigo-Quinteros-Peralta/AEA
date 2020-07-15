79d2218ddd06b9692240074ac306a1c1f330e039
IngridCortez <66339134+IngridCortez@users.noreply.github.com>
2020-07-15 00:53:29 -0500

    Add files via upload
    Apilar

--- Tarea 04 Stack and Queue + GIT/Trabajo1/src/Trabajo1.c
+++ Tarea 04 Stack and Queue + GIT/Trabajo1/src/Trabajo1.c
@@ -1,18 +1,3 @@
-/*
- ============================================================================
- Name        : Trabajo1.c
- Author      : Grupo 05
- Version     :
- Copyright   : Your copyright notice
- Description :
- • Implementar todas las operaciones vistas en la pila;
-• Implemente un programa principal que use la pila a través de un menú con los siguientes elementos: apilar, desapilar, limpiar, mostrar pila, salir del programa. Utilice el marco de programación del interruptor "C" para esto;
-• al mostrar la pila, el programa debe colocar su posición en el vector debajo de cada dato. Utilice las opciones de tamaño de campo de impresión printf () para esto;
-• la batería tendrá un tamaño máximo de 30, definida como una constante llamada MAXPILHA. Use esta constante para definir la estructura de datos que será la pila;
-• la pila será referenciada por una variable global.
- ============================================================================
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
@@ -24,6 +9,45 @@ struct Pila
     int tope;
     int datos[MAXPILA];
 };
-
 struct Pila apila;
 void menuPrincipal();
+
+int ERRORPILALLENA = -1;
+int ERRORPILAVACIA = -2;
+int apilar(int dato)
+{
+
+     if(pilaLlena())
+    {
+        printf("La pila esta llena \n");
+        return(ERRORPILALLENA);
+    }
+    else
+    {
+        printf("Ingrese el dato: \n");
+        scanf("%d", &dato);
+        apila.tope++;
+        apila.datos[apila.tope]=dato;
+
+    }
+}
+int mostrar ()
+{
+    int i = 0;
+    if(pilaVacia())
+    {
+        printf("La pila esta vacia \n");
+        return(ERRORPILAVACIA);
+    }
+    else
+    {
+        printf("Datos de la pila \n");
+
+        for(i=apila.tope; i>=0; i--)
+        {
+
+           printf("%d Su posicion es %d \n", apila.datos[i], i);
+        }
+    }
+}
+
