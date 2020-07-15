029d08884c12e288b58bb7e2586ca76ce39d9565
Rodrigo-Quinteros-Peralta <50123765+Rodrigo-Quinteros-Peralta@users.noreply.github.com>
2020-07-15 00:50:44 -0500

    Add files via upload

--- Tarea 04 Stack and Queue + GIT/Trabajo1/src/Trabajo1.c
+++ Tarea 04 Stack and Queue + GIT/Trabajo1/src/Trabajo1.c
@@ -1,18 +1,3 @@
-/*
- ============================================================================
- Name        : Trabajo1.c
- Author      : Grupo 05
- Version     :
- Copyright   : Your copyright notice
- Description :
- • Implementar todas las operaciones vistas en la pila;
-• Implemente un programa principal que use la pila a través de un menú con los siguientes elementos: apilar, desapilar, limpiar, mostrar pila, salir del programa. Utilice el marco de programación del interruptor "C" para esto;
-• al mostrar la pila, el programa debe colocar su posición en el vector debajo de cada dato. Utilice las opciones de tamaño de campo de impresión printf () para esto;
-• la batería tendrá un tamaño máximo de 30, definida como una constante llamada MAXPILHA. Use esta constante para definir la estructura de datos que será la pila;
-• la pila será referenciada por una variable global.
- ============================================================================
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
@@ -27,3 +12,26 @@ struct Pila
 
 struct Pila apila;
 void menuPrincipal();
+
+int desempilar()
+{
+    if(pilaVacia())
+    {
+        printf("ERROR: La pila esta vacia no intente desempilar!!!");
+        return(ERRORPILAVACIA);
+    }
+    else
+    {
+    	printf("SE DESEMPILO CORRECTAMENTE!!!");
+        apila.tope--;;
+
+    }
+}
+int limpiarPila()
+{
+apila.tope = -1;
+
+        printf("SE LIMPIO LA PILA. AHORA SE ECUENTRA VACIA!!!");
+
+}
+
