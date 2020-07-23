229b4e69923efaac0bdc268253aded179957a775
IngridCortez <66339134+IngridCortez@users.noreply.github.com>
2020-07-22 23:52:51 -0500

    Add files via upload

---
+++ T05-Stack and Queues/PE.9-PAG.251/Debug/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../PE9PAG251.c 
+
+OBJS += \
+./PE9PAG251.o 
+
+C_DEPS += \
+./PE9PAG251.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross GCC Compiler'
+	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
